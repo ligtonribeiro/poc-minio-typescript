e0d9e14e662285eebe0c0a73c8add9aa45c951d0
Wellington Menezes <ligton.ribeiro@gmail.com>
2022-08-05 19:19:53 -0300

    gera url assinada api MinIO Eyelab

--- config.minio.ts
+++ config.minio.ts
@@ -1,11 +1,11 @@
-import { ClientOptions, Client } from 'minio';
+import { ClientOptions, Client } from "minio";
 
 const minioClientOpt: ClientOptions = {
-  endPoint: 'localhost',
-  port: 9000,
-  useSSL: false,
-  accessKey: 'minio',
-  secretKey: 'minio123',
+  endPoint: "minio.appeyelab.com.br",
+  port: 443,
+  useSSL: true,
+  accessKey: "eyelabminio",
+  secretKey: "eyelab@2021",
 };
 
 export const minioClient = new Client({ ...minioClientOpt });
--- index.ts
+++ index.ts
@@ -1,27 +1,32 @@
-import { minioClient } from './config.minio';
-import { Request, Response } from 'express'
-import express from 'express';
+import { minioClient } from "./config.minio";
+import { Request, Response } from "express";
+import express from "express";
 
-const server = express()
-server.use(express.json())
+const server = express();
+server.use(express.json());
 
-server.post('/presignedUrl', async (req: Request, res: Response) => {
-    const file = req.body.name
-    console.log(file)
-    await minioClient.presignedPutObject('uploads', file, (err, url) => {
-        if (err) throw err
-        res.json(url)
-    })
-})
+server.post("/presignedUrl", async (req: Request, res: Response) => {
+  const file = req.body.name;
+  console.log(file);
+  await minioClient.presignedPutObject("uploads", file, (err, url) => {
+    if (err) throw err;
+    res.json(url);
+  });
+});
 
-server.get('/getPresignedUrl', async (req: Request, res: Response) => {
-    let file = req.query.file as string
-    await minioClient.presignedGetObject('uploads', file, (err, url) => {
-        if (err) throw err;
-        res.json(url)
-    })
-})
+server.get("/getPresignedUrl", async (req: Request, res: Response) => {
+  let file = req.query.file as string;
+  await minioClient.presignedGetObject(
+    "eyelab-bkt",
+    `dataCollector/${file}`,
+    48 * 60 * 60,
+    (err, url) => {
+      if (err) throw err;
+      res.json(url);
+    }
+  );
+});
 
 server.listen(8000, () => {
-    console.log('Server is running!!!!')
-})
+  console.log("🚀 Server is running!!!!");
+});
